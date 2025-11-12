88a5eee78a9599820da33020041a1a8c62ca3c9e
npmishra <124559101+npmishra@users.noreply.github.com>
2025-11-12 12:27:50 +0545

    Delete components directory

--- components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="mt-12 py-6 text-center text-sm text-gray-500">
-      © {new Date().getFullYear()} mishranarayan.com.np
-    </footer>
-  );
-}
